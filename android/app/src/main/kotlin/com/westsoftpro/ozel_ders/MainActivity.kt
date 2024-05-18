989dcfbb681c2a7bce027cd7493673588db1f05b
korkmazalibaran36 <korkmazalibaran36@gmail.com>
2024-05-18 18:38:59 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/westsoftpro/ozel_ders/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.westsoftpro.ozel_ders
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => HomePage(),
+      ),
+      GoRoute(
+        path: '/courses',
+        builder: (context, state) => CoursesPage(),
+      ),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            context.go('/courses');
+          },
+          child: Text('Go to Courses'),
+        ),
+      ),
+    );
+  }
+}
+
+class CoursesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Courses Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to Courses Page'),
+      ),
+    );
+  }
+}
