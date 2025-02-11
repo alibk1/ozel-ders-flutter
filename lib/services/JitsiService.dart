873fb5bf6ecab766e61b5902ea724b49ad29ea80
korkmazalibaran36 <korkmazalibaran36@gmail.com>
2025-02-11 04:20:51 +0300

    Jitsi sistemi düzenlendi - Eksik

--- lib/services/JitsiService.dart
+++ lib/services/JitsiService.dart
@@ -4,14 +4,20 @@ import 'package:http/http.dart' as http;
 class JitsiService {
   Future<String?> createMeeting() async {
     final url = Uri.parse('https://api.vitament.net/create-meeting');
-    final response = await http.post(url);
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"user_name": "user"}),
+    );
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      print(data['meeting_link']);
-      return data['meeting_link'];
+      print(data);
+      return data['admin_link'];
     } else {
+      print("Error: ${response.statusCode}");
       return null;
     }
   }
+
 }
